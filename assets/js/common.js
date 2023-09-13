c991d0461557943cb3c46074da8efa204b1f9d07
George <31376482+george-gca@users.noreply.github.com>
2023-09-13 12:42:38 -0300

    Update hide/show tldr

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
   $('a.abstract').click(function() {
     $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
   });
   $('a.bibtex').click(function() {
-    $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
     });
   $('a.tldr').click(function() {
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
   });
   $('a').removeClass('waves-effect waves-light');
