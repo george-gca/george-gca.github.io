7be91d41608998589d4d154ca37568a618922001
George Araújo <george.gcac@gmail.com>
2024-03-05 18:28:50 -0300

    Testing plugin
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- _plugins/sliding-enter-animation.rb
+++ _plugins/sliding-enter-animation.rb
@@ -35,18 +35,46 @@ require "nokogiri"
 # end
 
 # Jekyll::Hooks.register([:pages, :posts], :post_render) do |page|
-Jekyll::Hooks.register([:pages, :posts], :post_convert) do |page|
-    # only do this if page is an html or md page
-    if page.path.end_with?("about.md")
-        Jekyll.logger.info "!!!!!!!!!!! Page before:\n#{page.content}"
+# # Jekyll::Hooks.register([:pages, :posts], :post_convert) do |page|
+#     # only do this if page is an html or md page
+#     if page.path.end_with?("about.md")
+#         Jekyll.logger.info "!!!!!!!!!!! Page before:\n#{page.output}"
 
+#         if page.path.end_with?(".html", ".md")
+#             noko = Nokogiri::HTML(page.output)
+#             # noko = Nokogiri::HTML::fragment(page.content)
+#             last_index = 0
+#             Jekyll.logger.info "Trying to add animations to page #{page.path}"
+#             noko.css("audio", "div.card", "div.highlighter-rouge", "figure", "h2", "h3", "p", "video").each_with_index do |tag, index|
+#                 Jekyll.logger.info "\tAdded to: #{tag.name}"
+#                 tag[:style] = (tag[:style] || "") + "--stagger: #{index+1};"
+#                 tag[:data_animate] = ""
+#                 if index == 10 then
+#                     break
+#                 end
+#                 last_index = index + 1
+#             end
+
+#             page.output  = noko.to_html
+#             # page.content  = noko.to_html
+#             Jekyll.logger.info "Added #{last_index} animations to page #{page.path}"
+
+#             if page.path.end_with?("about.md")
+#                 Jekyll.logger.info "!!!!!!!!!!! Page after:\n#{page.output}"
+#             end
+#         else
+#             Jekyll.logger.info "Not adding animation to #{page.data['ext']} page #{page.path}"
+#         end
+#     end
+# end
+
+Jekyll::Hooks.register([:pages, :posts], :post_render) do |page|
+    # Jekyll::Hooks.register([:pages, :posts], :post_convert) do |page|
         if page.path.end_with?(".html", ".md")
-            # noko = Nokogiri::HTML(page.output)
-            noko = Nokogiri::HTML::fragment(page.content)
+            noko = Nokogiri::HTML(page.output)
+            # noko = Nokogiri::HTML::fragment(page.content)
             last_index = 0
-            Jekyll.logger.info "Trying to add animations to page #{page.path}"
             noko.css("audio", "div.card", "div.highlighter-rouge", "figure", "h2", "h3", "p", "video").each_with_index do |tag, index|
-                Jekyll.logger.info "\tAdded to: #{tag.name}"
                 tag[:style] = (tag[:style] || "") + "--stagger: #{index+1};"
                 tag[:data_animate] = ""
                 if index == 10 then
@@ -55,15 +83,10 @@ Jekyll::Hooks.register([:pages, :posts], :post_convert) do |page|
                 last_index = index + 1
             end
 
-            # page.output  = noko.to_html
-            page.content  = noko.to_html
-            Jekyll.logger.info "Added #{last_index} animations to page #{page.path}"
+            page.output  = noko.to_html
+            # page.content  = noko.to_html
 
-            if page.path.end_with?("about.md")
-                Jekyll.logger.info "!!!!!!!!!!! Page after:\n#{page.content}"
-            end
         else
             Jekyll.logger.info "Not adding animation to #{page.data['ext']} page #{page.path}"
         end
     end
-end
