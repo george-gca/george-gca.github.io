ccd8f92066556f49561e601581fa3a84fe0dd65a
George Araujo <george.gcac@gmail.com>
2024-03-05 18:32:01 -0300

    Added blockquote as element to be animated
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- _plugins/sliding-enter-animation.rb
+++ _plugins/sliding-enter-animation.rb
@@ -3,7 +3,7 @@ require "nokogiri"
 Jekyll::Hooks.register([:pages, :posts], :post_render) do |page|
     if page.path.end_with?(".html", ".md")
         noko = Nokogiri::HTML(page.output)
-        noko.search("audio, div.card, div.highlighter-rouge, div.news, div.repositories, div.row, div.social, div.tag-category-list, figure, h2, h3, p, video").each_with_index do |tag, index|
+        noko.search("audio, blockquote, div.card, div.highlighter-rouge, div.news, div.repositories, div.row, div.social, div.tag-category-list, figure, h2, h3, p, video").each_with_index do |tag, index|
             tag[:style] = (tag[:style] || "") + "--stagger: #{index+1};"
             tag["data-animate"] = ""
             # stop after 20 elements
