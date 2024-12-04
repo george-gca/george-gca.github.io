933e23c53d91485dd0848d51c43d0d390456a5cf
George Araújo <george.gcac@gmail.com>
2024-12-04 15:28:57 -0300

    Cleaned animation plugin code
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- _plugins/sliding-enter-animation.rb
+++ _plugins/sliding-enter-animation.rb
@@ -1,89 +1,63 @@
+# based on the excellent post by Anthony Fu: https://antfu.me/posts/sliding-enter-animation
 require "nokogiri"
 
-Jekyll::Hooks.register([:pages, :posts], :post_render) do |page|
-    if page.site.config.key?("animation")
+Jekyll::Hooks.register([:pages, :documents], :post_render) do |page|
+    if page.site.config.key?("animation") and page.site.config["animation"].key?("enabled") and page.site.config["animation"]["enabled"]
         config = page.site.config["animation"]
+        verbose = config["verbose"] || false
 
-        if config.key?("enabled") and config["enabled"]
-            if config.key?("verbose")
-                verbose = config["verbose"]
-            else
-                verbose = false
+        if page.path.end_with?(".html", ".md")
+            # add animation to html elements as style attribute
+            noko = Nokogiri::HTML(page.output)
+
+            # set default values
+            limit = config["max_elements"] || 20
+            selector = config["selector"] ||
+                "audio, blockquote, div.card, div.highlighter-rouge, div.news, div.repositories, div.row, div.social, div.tag-category-list, figure, h2, h3, p, tr, video"
+
+            noko.search(selector).each_with_index do |tag, index|
+                tag[:style] = (tag[:style] || "") + "--stagger: #{index+1};"
+                tag["data-animate"] = ""
+                if index == limit then
+                    break
+                end
             end
 
-            if page.path.end_with?(".html", ".md")
-                noko = Nokogiri::HTML(page.output)
-
-                if config.key?("selector")
-                    selector = config["selector"]
-                else
-                    selector = "audio, blockquote, div.card, div.highlighter-rouge, div.news, div.repositories, div.row, div.social, div.tag-category-list, figure, h2, h3, p, tr, video"
-                end
-
-                if config.key?("max_elements")
-                    limit = config["max_elements"]
-                else
-                    limit = -1
-                end
-
-                noko.search(selector).each_with_index do |tag, index|
-                    tag[:style] = (tag[:style] || "") + "--stagger: #{index+1};"
-                    tag["data-animate"] = ""
-                    if index == limit then
-                        break
-                    end
-                end
-
-                page.output  = noko.to_html
-                if verbose
-                    Jekyll.logger.info "Added animation to page #{page.path}"
-                end
-
-            elsif page.path.end_with?(".scss")
-                if config.key?("delay")
-                    delay = config["delay"]
-                else
-                    delay = "100ms"
-                end
-
-                if config.key?("name")
-                    name = config["name"]
-                else
-                    name = "enter"
-                end
-
-                if config.key?("from")
-                    from = config["from"]
-                    # transform value to string, if it is a hash
-                    if from.is_a?(Hash)
-                        from = from.map { |k, v| "#{k}:#{v}" }.join(";")
-                    end
-                else
-                    from = "opacity:0;transform:translateY(10px)"
-                end
-
-                if config.key?("to")
-                    to = config["to"]
-                    # transform value to string, if it is a hash
-                    if to.is_a?(Hash)
-                        to = to.map { |k, v| "#{k}:#{v}" }.join(";")
-                    end
-                else
-                    to = "opacity:1;transform:none"
-                end
-
-                css = page.output
-                css += "@keyframes #{name}{from{#{from}}to{#{to}}}[data-animate]{--stagger:0;--delay:#{delay};--start:0.1ms}
-@media(prefers-reduced-motion:no-preference){[data-animate]{animation:#{name} .6s both;animation-delay: calc(var(--stagger) * var(--delay) + var(--start))}}"
-                page.output = css
-
-                if verbose
-                    puts "Added animation to css file #{page.path}"
-                end
-
-            elsif verbose
-                Jekyll.logger.info "Skipped adding animation to page #{page.path}"
+            page.output = noko.to_html
+            if verbose
+                Jekyll.logger.info "Added animation to page #{page.path}"
             end
+
+        elsif page.path.end_with?(".scss")
+            # add animation config to css file
+            # set default values
+            delay = config["delay"] || "100ms"
+            duration = config["duration"] || "0.6s"
+            name = config["name"] || "enter"
+
+            from = config["from"] || "opacity:0;transform:translateY(10px)"
+            # transform value to string, if it is a hash
+            if from.is_a?(Hash)
+                from = from.map { |k, v| "#{k}:#{v}" }.join(";")
+            end
+
+            to = config["to"] || "opacity:1;transform:none"
+            # transform value to string, if it is a hash
+            if to.is_a?(Hash)
+                to = to.map { |k, v| "#{k}:#{v}" }.join(";")
+            end
+
+            css = page.output
+            css += "@keyframes #{name}{from{#{from}}to{#{to}}}[data-animate]{--stagger:0;--delay:#{delay};--start:0.1ms}
+@media(prefers-reduced-motion:no-preference){[data-animate]{animation:#{name} #{duration} both;animation-delay: calc(var(--stagger) * var(--delay) + var(--start))}}"
+            page.output = css
+
+            if verbose
+                Jekyll.logger.info "Added animation to css file #{page.path}"
+            end
+
+        elsif verbose
+            Jekyll.logger.info "Skipped adding animation to file #{page.path}"
         end
     end
 end
